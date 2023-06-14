8c6bbcc477749d82df4bc57f21fa67e736529a54
AnonyBunny <106082877+AnonyBunny@users.noreply.github.com>
2023-06-14 11:40:17 +0300

    Add files via upload

--- CLECKER/Scripts/ClickerButton.gd
+++ CLECKER/Scripts/ClickerButton.gd
@@ -8,10 +8,11 @@ func _process(_delta):
 		return
 	if Input.is_action_just_pressed("click"):  
 		points += 1
-		print(points)
+	$Label.text = str(points)
 
-#todo add a store, add a point counter
-#this was way easier than I expected tbh
+#todo add a store
+#todo save system
+#todo automated clicking
 
 func _on_Area2D_mouse_entered():
 	areastate = true
